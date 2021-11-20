4b00833954c512b4f1efc8c83226a0df1a0c91d4
David Däster <david@daester.net>
2021-11-20 23:35:41 +0100

    First rough layout

--- src/Main.elm
+++ src/Main.elm
@@ -3,7 +3,7 @@ module Main exposing (..)
 import Browser
 import Framework.Typography exposing (h1)
 import Html exposing (Html)
-import Element exposing (Color, Element, alignRight, centerX, centerY, clip, column, el, fill, height, image, layout, maximum, minimum, padding, px, rgb, rgb255, row, spacing, text, width)
+import Element exposing (Color, Element, alignBottom, alignLeft, alignRight, alignTop, centerX, centerY, clip, column, el, fill, height, image, layout, maximum, minimum, padding, px, rgb, rgb255, row, spacing, text, width)
 import Element.Background as Background
 import Element.Border as Border
 import Element.Font as Font
@@ -20,20 +20,29 @@ main =
         , subscriptions = always Sub.none
         }
 
----- VIEW ----
-
-
 view : Model -> Html Msg
 view model = layout [ width fill, height fill ] <|
                      column
-                         [ centerX
-                         , spacing 20
+                         [ spacing 20
+                         , width fill
+                         , height fill
                          ]
-                         [ image [width <| px 200]
-                             { src = "./logo.svg"
-                             , description = "An image"
-                             }
-                         , h1 [] <| text "Heading"
+                         [ row [padding 20, spacing 20, width fill]
+                          [ el [alignRight] <| text "Link 1"
+                          , el [alignRight] <| text "Link 2"
+                          ]
+                         , row [padding 20, width fill, height fill]
+                           [ el [alignLeft, alignTop] <| text "Left status column"
+                           , el [centerX, centerY] <| text "Game Area"
+                           , el [alignRight, alignTop] <| text "Right status column"
+                           ]
+
+                         , row [padding 20, width fill, height <| px 100, alignBottom]
+                           [ el [alignLeft] <| text "Left footer"
+                           , el [centerX] <| text "center footer"
+                           , el [alignRight] <| text "right footer"
+                           ]
+
                          ]
 white : Color
 white =
