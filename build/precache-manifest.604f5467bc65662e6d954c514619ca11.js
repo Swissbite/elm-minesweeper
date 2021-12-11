7a9a480e56b35d3cfd52197656fff2b4553cab13
David Däster <david@daester.net>
2021-12-11 14:38:17 +0100

    Initial gh-pages branch with current running applicaiton

---
+++ build/precache-manifest.604f5467bc65662e6d954c514619ca11.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12354f9f4a90252ebc7ff31949a83be",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "7c35dcb7354721d11a21a42ec3f78c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b055f2d0254f66f7de4910b0673e8d7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "446428ac1b613b13aa10",
+    "url": "/static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "446428ac1b613b13aa10",
+    "url": "/static/js/main.446428ac.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "/static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
