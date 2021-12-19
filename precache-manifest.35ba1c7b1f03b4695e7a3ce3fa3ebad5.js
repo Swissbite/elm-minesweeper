b54d2ec9e521c092406b71ec544e1e6b6d1ad300
Swissbite <Swissbite@users.noreply.github.com>
2021-12-19 00:15:39 +0000

    deploy: d6bbefe75d88170ecebdb387c4bdc198ec084f72

---
+++ precache-manifest.35ba1c7b1f03b4695e7a3ce3fa3ebad5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12354f9f4a90252ebc7ff31949a83be",
+    "url": "./favicon.ico"
+  },
+  {
+    "revision": "415a0df7692dcab4dc7592b104d0ba1c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b055f2d0254f66f7de4910b0673e8d7",
+    "url": "./logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "./manifest.json"
+  },
+  {
+    "revision": "f28306cb0c134e699ebd",
+    "url": "./static/css/main.0af5c45f.chunk.css"
+  },
+  {
+    "revision": "f28306cb0c134e699ebd",
+    "url": "./static/js/main.f28306cb.chunk.js"
+  },
+  {
+    "revision": "d5752d2fdce3c7e150f5",
+    "url": "./static/js/runtime~main.d5752d2f.js"
+  },
+  {
+    "revision": "0b49ec49a892799f59f3",
+    "url": "./static/js/vendors~main.0b49ec49.chunk.js"
+  }
+]);
